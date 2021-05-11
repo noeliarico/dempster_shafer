1176b0ba2bb9936ba5f4ad0080b6bd85d583c599
Noelia Rico <noeliarico@uniovi.es>
2021-05-11 10:02:49 +0200

    Add syntax to check Sphinx

--- src/dempster_shafer/__init__.py
+++ src/dempster_shafer/__init__.py
@@ -1,4 +1,15 @@
+"""A package from dempster shafer.
+
+.. module:: dempster_shafer
+
+.. moduleauthor:: Noelia Rico <noeliarico@uniovi.es>
+.. moduleauthor:: Luigi Troiano <ltroiano@unisa.it>
+
+"""
+
 from .lattice import Lattice
 
 def hello():
+    """This is an example function...
+    """
     print("Ciao tutti!")
