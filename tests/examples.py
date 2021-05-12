d7eea95e18f1cd7cf0b58f7ea15c368138dffcf3
Noelia Rico <noeliarico@uniovi.es>
2021-05-12 13:17:50 +0200

    Add file for examples

--- tests/examples.py
+++ tests/examples.py
@@ -2,8 +2,23 @@
 import dempster_shafer as ds
 
 # Create a Frame of Discerment for the items a, b, c, d using a list
-fod = ds.FrameOfDiscernment(['a', 'b', 'c', 'd'])
+fod1 = ds.FrameOfDiscernment(['a', 'b', 'c', 'd'])
+print(fod1.get_index('ad'))
+#fod1.print_all()
+fs1 = ds.FocalSet(fod1, 
+    {
+        "abc": 0.4,
+        "abdc": 0.3,
+        "a": 0.3
+    })
+#print(fs1)
+print(fod1.is_subset('abd'))
+print(fod1.is_subset('aed'))
 
-# Create a Frame of Discerment for the items a, b, c, d using a numpy
-import numpy as np
-fod = ds.FrameOfDiscernment(np.array(['a', 'b', 'c', 'd']))
+# fod2 = ds.FrameOfDiscernment(['a', 'b', 'c', 'd', 'e', 'f'])
+# fod2.print_all()
+
+# # Create a Frame of Discerment for the items a, b, c, d using a numpy
+# import numpy as np
+# fod2 = ds.FrameOfDiscernment(np.array(['a', 'b', 'c', 'd']))
+
