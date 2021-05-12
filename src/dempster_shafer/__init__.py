766c7e78475e95b50b942bc43ee8bde5a966da32
Noelia Rico <noeliarico@uniovi.es>
2021-05-12 13:16:53 +0200

    Load Focal Set from the root

--- src/dempster_shafer/__init__.py
+++ src/dempster_shafer/__init__.py
@@ -1,13 +1,12 @@
 """A package from dempster shafer.
 
-.. module:: dempster_shafer
-
 .. moduleauthor:: Noelia Rico <noeliarico@uniovi.es>
 .. moduleauthor:: Luigi Troiano <ltroiano@unisa.it>
 
 """
 
 from .fod import FrameOfDiscernment
+from .fs import FocalSet
 from .lattice import Lattice
 
 
